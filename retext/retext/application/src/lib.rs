96c084946c2824e6c2dbb50271da71e379cf798d
Kawano Kenichi <bppk28@gmail.com>
2021-07-06 15:20:38 +0000

    :sparkles: Add application service
    生文字から絵文字に変換するサービスを追加する。

--- retext/retext/application/src/lib.rs
+++ retext/retext/application/src/lib.rs
@@ -1,7 +1,69 @@
+use domain::convert_service;
+use domain::emoji::emoji_alphabet::Color;
+use domain::raw::raw_text::RawText;
+
+#[derive(Debug)]
+pub struct InputData {
+    pub raw_text: String,
+    pub color: Color,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct OutputData {
+    pub emoji_text: String,
+}
+
+pub fn raw2emoji(input: InputData) -> OutputData {
+    let raw_text = RawText::from(input.raw_text.as_str());
+    let color = input.color;
+
+    let result = convert_service::convert(raw_text, color);
+
+    OutputData {
+        emoji_text: result.get().iter().map(|e| e.get()).collect(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_convert_to_white() {
+        let actual = raw2emoji(InputData {
+            raw_text: String::from("abc"),
+            color: Color::White,
+        });
+        let expected = OutputData {
+            emoji_text: String::from(
+                [
+                    ":alphabet-white-a:",
+                    ":alphabet-white-b:",
+                    ":alphabet-white-c:",
+                ]
+                .join(""),
+            ),
+        };
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_convert_to_yellow() {
+        let actual = raw2emoji(InputData {
+            raw_text: String::from("xyz"),
+            color: Color::Yellow,
+        });
+        let expected = OutputData {
+            emoji_text: String::from(
+                [
+                    ":alphabet-yellow-x:",
+                    ":alphabet-yellow-y:",
+                    ":alphabet-yellow-z:",
+                ]
+                .join(""),
+            ),
+        };
+        assert_eq!(actual, expected);
     }
 }
