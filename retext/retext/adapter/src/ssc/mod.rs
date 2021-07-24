c45fa345c41a5ac67d4b6afb9ac3ae780e72aafd
Kawano Kenichi <bppk28@gmail.com>
2021-07-24 12:31:23 +0000

    :art: Use the request_parser module
    リクエスト受付部で `text_parse` を使用するように修正する。
    また、セパレータを使用してレスポンスの文字列（ハイライトとメッセージ）を結合する。

--- retext/retext/adapter/src/ssc/mod.rs
+++ retext/retext/adapter/src/ssc/mod.rs
@@ -1,3 +1,5 @@
+mod request_parser;
+
 use application as app;
 use serde::Deserialize;
 
@@ -30,21 +32,130 @@ pub mod text {
         }
 
         pub fn post(req: Request) -> Result<Response, std::io::Error> {
-            let mut s = req.text.split_whitespace();
-
-            let highlight = String::from(s.next().expect("Highlight text is necessary"));
-            let color = s.next();
-            let message = s.collect::<Vec<_>>().join(" ");
+            let request_parser::ParsedText {
+                highlight,
+                color,
+                message,
+                separator
+            } = request_parser::text_parse(req.text);
 
             let input_data = app::InputData {
                 highlight,
                 color,
-                message: Some(message.as_str()),
+                message,
+                separator,
             };
 
             Ok(Response {
                 value: app::highlight_message(input_data).highlighted_message,
             })
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn request_factory(s: &str) -> Request {
+        Request {
+            text: String::from(s),
+            api_app_id: String::new(),
+            channel_id: String::new(),
+            channel_name: String::new(),
+            command: String::new(),
+            is_enterprise_install: false,
+            response_url: String::new(),
+            team_domain: String::new(),
+            team_id: String::new(),
+            token: String::new(),
+            trigger_id: String::new(),
+            user_id: String::new(),
+            user_name: String::new(),
+        }
+    }
+
+    #[test]
+    // text の第 1 引数のみ指定する
+    fn test_post_text_only_first_arg() {
+        let req = request_factory("a");
+
+        let v = if let Ok(res) = text::highlight::post(req) {
+            res.value
+        } else {
+            String::new()
+        };
+
+        assert_eq!(v, ":alphabet-white-a:");
+    }
+
+    #[test]
+    // text の第 2 引数に white を指定する
+    fn test_post_text_second_arg_white() {
+        let req = request_factory("a white red");
+
+        let v = if let Ok(res) = text::highlight::post(req) {
+            res.value
+        } else {
+            String::new()
+        };
+
+        assert_eq!(v, ":alphabet-white-a: red");
+    }
+
+    #[test]
+    // text の第 2 引数に yellow を指定する
+    fn test_post_text_second_arg_yellow() {
+        let req = request_factory("a yellow red");
+
+        let v = if let Ok(res) = text::highlight::post(req) {
+            res.value
+        } else {
+            String::new()
+        };
+
+        assert_eq!(v, ":alphabet-yellow-a: red");
+    }
+
+    #[test]
+    // text の第 2 引数に white / yellow 以外を指定する
+    fn test_post() {
+        let req = request_factory("a blue red");
+
+        let v = if let Ok(res) = text::highlight::post(req) {
+            res.value
+        } else {
+            String::new()
+        };
+
+        assert_eq!(v, ":alphabet-white-a: blue red");
+    }
+
+    #[test]
+    // text に改行がある
+    fn test_post_text_contains_linefeed() {
+        let req = request_factory("a yellow\nred");
+
+        let v = if let Ok(res) = text::highlight::post(req) {
+            res.value
+        } else {
+            String::new()
+        };
+
+        assert_eq!(v, ":alphabet-yellow-a:\nred");
+    }
+
+    #[test]
+    // text の第 2 引数に当たる部分の手前に改行がある
+    fn test_post_text_contains_linefeed_in_second_arg() {
+        let req = request_factory("a\nyellow\nred");
+
+        let v = if let Ok(res) = text::highlight::post(req) {
+            res.value
+        } else {
+            String::new()
+        };
+
+        assert_eq!(v, ":alphabet-white-a:\nyellow\nred");
     }
 }
--- retext/retext/application/src/lib.rs
+++ retext/retext/application/src/lib.rs
@@ -3,10 +3,11 @@ pub use domain::emoji::emoji_alphabet::Color;
 use domain::raw::raw_text::RawText;
 
 #[derive(Debug)]
-pub struct InputData<'a> {
+pub struct InputData {
     pub highlight: String,
-    pub color: Option<&'a str>,
-    pub message: Option<&'a str>,
+    pub color: Option<String>,
+    pub message: Option<String>,
+    pub separator: Option<String>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -23,12 +24,17 @@ pub fn highlight_message(input: InputData) -> OutputData {
         },
         None => Color::White,
     };
+    let separator = input.separator.unwrap_or_default();
     let message = input.message.unwrap_or_default();
 
-    let result = convert_service::convert(highlight, color);
+    let result = convert_service::convert(highlight, color)
+        .get()
+        .iter()
+        .map(|e| e.get())
+        .collect::<String>();
 
     OutputData {
-        highlighted_message: result.get().iter().map(|e| e.get()).collect::<String>() + &message,
+        highlighted_message: format!("{}{}{}", result, separator, message),
     }
 }
 
@@ -40,8 +46,9 @@ mod tests {
     fn test_convert_to_white() {
         let actual = highlight_message(InputData {
             highlight: String::from("abc"),
-            color: Some("white"),
+            color: Some(String::from("white")),
             message: None,
+            separator: None,
         });
         let expected = OutputData {
             highlighted_message: String::from(
@@ -61,8 +68,9 @@ mod tests {
     fn test_convert_to_yellow() {
         let actual = highlight_message(InputData {
             highlight: String::from("xyz"),
-            color: Some("YeLLow"),
+            color: Some(String::from("YeLLow")),
             message: None,
+            separator: None,
         });
         let expected = OutputData {
             highlighted_message: String::from(
@@ -82,7 +90,8 @@ mod tests {
         let actual = highlight_message(InputData {
             highlight: String::from("xyz"),
             color: None,
-            message: Some("test message"),
+            message: Some(String::from("test message")),
+            separator: Some(String::from(" ")),
         });
         let expected = OutputData {
             highlighted_message: String::from(
@@ -92,7 +101,7 @@ mod tests {
                     ":alphabet-white-z:",
                 ]
                 .join(""),
-            ) + "test message",
+            ) + " test message",
         };
         assert_eq!(actual, expected);
     }
